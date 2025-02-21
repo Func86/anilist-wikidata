fbbde6f18dd73b676efe2b9efe3b0af9d827be85
Func <Funcer@outlook.com>
2025-02-21 23:52:47 +0800

    feat: Match staff by name and date of birth

--- tools/kanji-labels.js
+++ tools/kanji-labels.js
@@ -1,7 +1,7 @@
 import fs from 'node:fs';
 
 import { SPARQLQueryDispatcher } from '../utils/SPARQLQueryDispatcher.js';
-import waseiKanji from './Wasei-Kanji.json' with { type: 'json' };
+import { replaceWaseiKanji } from './Wasei-Kanji.js';
 
 const dispatcher = new SPARQLQueryDispatcher();
 const queryTemplate = fs.readFileSync('./kanji-labels.rq', 'utf8');
@@ -17,7 +17,7 @@ for (const propId in properties) {
 	const data = [];
 	for (const { item, jaLabel } of response.results.bindings) {
 		const id = item.value.split('/').pop();
-		const label = replaceBulk(jaLabel.value, waseiKanji);
+		const label = replaceWaseiKanji(jaLabel.value);
 		data.push([ id, 'Lzh', `"${label}"` ]);
 	}
 
@@ -27,19 +27,4 @@ for (const propId in properties) {
 	} else if (fs.existsSync(dataFile)) {
 		fs.unlinkSync(dataFile);
 	}
-}
-
-/**
- * Replaces multiple substrings in a given string based on a replacement map.
- *
- * @param {string} str - The original string where replacements will be made.
- * @param {Object} replaceMap - An object where keys are substrings to be replaced and values are their replacements.
- * @returns {string} - The modified string with all replacements made.
- */
-function replaceBulk(str, replaceMap) {
-	const regex = Object.keys(replaceMap).map(
-		// Escape special characters for regex
-		(key) => key.replace(/([-[\]{}()*+?.\\^$|#,])/g, '\\$1')
-	);
-	return str.replace(new RegExp(regex.join('|'), 'g'), matched => replaceMap[matched]);
 }
