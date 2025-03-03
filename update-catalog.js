ea9ad8294ee309cdef196f1170991a680aac39f4
Func <Funcer@outlook.com>
2025-03-03 21:38:59 +0800

    feat: Incremental update of catalogs

--- update-catalog.js
+++ update-catalog.js
@@ -6,6 +6,7 @@ const catalogName = process.argv[2];
 const graphqlQuery = fs.readFileSync(`./catalogs/${catalogName}.graphql`, 'utf8');
 const { entryCallback } = await import(`./catalogs/${catalogName}.js`);
 
-const pageOffset = parseInt(process.argv[3]) || 0;
-const updater = new CatalogUpdater(catalogName, graphqlQuery, entryCallback, pageOffset);
-await updater.update();
+const updateType = process.argv[3] || 'incremental';
+const pageOffset = parseInt(process.argv[4]) || 0;
+const updater = new CatalogUpdater(catalogName, graphqlQuery, entryCallback);
+await updater.update(updateType, pageOffset);
