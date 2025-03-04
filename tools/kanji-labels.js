c9988f681d7b046aafa97cf4bc2d453aa76d2674
Func <Funcer@outlook.com>
2025-03-04 22:08:58 +0800

    feat: Write Kanji labels to the same file

--- tools/kanji-labels.js
+++ tools/kanji-labels.js
@@ -7,24 +7,26 @@ const dispatcher = new SPARQLQueryDispatcher();
 const queryTemplate = fs.readFileSync('./kanji-labels.rq', 'utf8');
 
 const properties = {
-	'P11227': 'staff',
-	'P11736': 'character',
+	P11227: 'staff',
+	P11736: 'characters',
 };
+const data = [];
 for (const propId in properties) {
+	console.log(`Processing ${properties[propId]}...`);
 	const sparqlQuery = queryTemplate.replace('<PROPERTY>', propId);
 	const response = await dispatcher.query(sparqlQuery);
 
-	const data = [];
 	for (const { item, jaLabel } of response.results.bindings) {
 		const id = item.value.split('/').pop();
 		const label = replaceWaseiKanji(jaLabel.value);
 		data.push([ id, 'Lzh', `"${label}"` ]);
 	}
+	data.push([]);
+}
 
-	const dataFile = `./kanji-labels-${properties[propId]}.tsv`;
-	if (data.length) {
-		fs.writeFileSync(dataFile, data.map((row) => row.join('\t')).join('\n') );
-	} else if (fs.existsSync(dataFile)) {
-		fs.unlinkSync(dataFile);
-	}
+const dataFile = `./kanji-labels.tsv`;
+if (data.length - 2 > 0) {
+	fs.writeFileSync(dataFile, data.map((row) => row.join('\t')).join('\n'));
+} else if (fs.existsSync(dataFile)) {
+	fs.unlinkSync(dataFile);
 }
